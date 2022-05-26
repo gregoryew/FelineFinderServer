60a9e79b321b114ecd33a584b55c93ecf6534d94
Gregory Williams <gregoryew@gmail.com>
2022-05-25 22:16:52 -0700

    rearranged code

--- app.js
+++ app.js
@@ -9,10 +9,58 @@ app.use("/", router);
 
 const mysql = require('mysql');
 const { fstat } = require("fs");
+const { nextTick } = require("process");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
+
+app.post("/isFavorite", (req, res) => {
+    try {
+        console.debug("isFavorite Start");
+
+        const conn = mysql.createConnection({
+            host: process.env.DBHOST,
+            port: process.env.DBPORT,
+            user: process.env.DBUSER,
+            database: process.env.DBNAME,
+            ssl: {
+                ca: fs.readFileSync('ca-certificate.crt'),
+            }
+        });
+
+        console.log("isFavorite logged on to db");
+
+        let selectQuery = 'SELECT COUNT(*) c FROM Favorites WHERE userID = ? AND petID = ?';
+        let query = mysql.format(selectQuery,[req.body.userid, req.body.petid]);
+        conn.query(query,(err, response, fields) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send("Error querying database.");
+            }
+            // rows added
+            console.log("isFavorite");
+            console.log(response.length);
+            if (response.length > 0) {
+                console.log(JSON.stringify(response));
+                res.status(200).json({IsFavorite: response[0].c >= 1});
+            } else {
+                res.status(200).json({IsFavorite: false});
+            }
+    });
+    } catch (err) {
+        next(err);
+    } finally {
+        conn.end();
+    }
+});
+
+let PORT = process.env.PORT || 3000;
+let IP = process.env.IP || '127.0.0.1';
+app.listen(PORT, IP, () => {
+    console.log('Server is running at port ' + PORT + ' and IP = ' + IP);
+});
+
 /*
 const pool = mysql.createPool({
   connectionLimit : 75,
@@ -68,18 +116,6 @@ function unfavorite(data) {
     });
 }
 
-let PORT = process.env.PORT || 3000;
-let IP = process.env.IP || '127.0.0.1';
-app.listen(PORT, IP, () => {
-    console.log('Server is running at port ' + PORT + ' and IP = ' + IP);
-});
-
-/*
-app.listen(3000, () => {
-    console.log('Server is running at port 3000');
-});
-*/
-
 app.post("/addUser",(req,res) => {
     //pool.getConnection((err, connection) => {
         //if(err) throw err;
@@ -90,49 +126,7 @@ app.post("/addUser",(req,res) => {
     //});
 });
 
-app.post("/isFavorite", (req, res) => {
-    //pool.getConnection(async (err, connection) => {
-        //if(err) throw err;
-
-        console.log("isFavorite Start");
-
-        const conn = mysql.createConnection({
-            host: 'feline-finder-do-user-11649465-0.b.db.ondigitalocean.com',
-            port: '25060',
-            user: 'felinefinder',
-            password: 'AVNS_auc4rgZi5-p28fr',
-            database: 'defaultdb',
-            ssl: {
-                ca: fs.readFileSync('ca-certificate.crt'),
-            }
-        });
-
-        console.log("isFavorite logged on to db");
-
-        let selectQuery = 'SELECT COUNT(*) c FROM Favorites WHERE userID = ? AND petID = ?';
-        let query = mysql.format(selectQuery,[req.body.userid, req.body.petid]);
-        conn.query(query,(err, response, fields) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send("Error querying database.");
-            }
-            // rows added
-            console.log("isFavorite");
-            console.log(response.length);
-            if (response.length > 0) {
-                console.log(JSON.stringify(response));
-                res.status(200).json({IsFavorite: response[0].c >= 1});
-            } else {
-                res.status(200).json({IsFavorite: false});
-            }
-        //});
-    });
-    conn.end();
-});
-
 app.post("/favorite", (req, res) => {
-    //pool.getConnection((err, connection) => {
-        //if(err) throw err;
         console.log("GOT HERE");
         console.log(req.body);
 
