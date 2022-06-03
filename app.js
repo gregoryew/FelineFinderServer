49c659bc41399fe63f41f8e9bcccb3a9e7af2200
Gregory Williams <gregoryew@gmail.com>
2022-06-03 08:37:31 -0700

    change status position

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ app.post("/isFavorite", (req, res) => {
             else
             {
                 if (response.length > 0) {
-                    response.status(200).json({IsFavorite: response[0].c >= 1});
+                    response.json({IsFavorite: response[0].c >= 1}).status(200);
                 } else {
-                    response.status(200).json({IsFavorite: false});
+                    response.json({IsFavorite: false}).status(200);
                 }
             }
         });
