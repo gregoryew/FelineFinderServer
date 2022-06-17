770e1f77d9973d25980c9bbd6413b2aa43af6f57
Gregory Williams <gregoryew@gmail.com>
2022-06-17 15:08:58 -0700

    favorite now first delete the user row then insert

--- app.js
+++ app.js
@@ -17,22 +17,25 @@ function getConn() {
         user: process.env.DBUSER || "FelineFinder",
         database: process.env.DBNAME || "defaultdb",
         password: process.env.DBPASSWORD || "AVNS_3Sfkz7FXwS6OCC0hSHC"
-        //ssl  : {
-        //    ca : fs.readFileSync(__dirname + '/ca-certificate.crt')
-        //}
     });
 }
 
 app.post("/isFavorite", (req, res) => {
     try {
+        let dir = path.join(__dirname, '/log.txt');
+        fs.appendFileSync(dir, 'entered /isFavorite \n');
         const conn = getConn();
+        fs.appendFileSync(dir, 'got conn \n');
         conn.connect();
+        fs.appendFileSync(dir, 'connected to db \n');
 
         let selectQuery = 'SELECT COUNT(*) c FROM Favorites WHERE userID = ? AND petID = ?';
         let query = mysql.format(selectQuery,[req.body.userid, req.body.petid]);
         conn.query(query,(err, response) => {
+            fs.appendFileSync(dir, 'ran query ' + query + '\n');
+
             if(err) {
-                res.status(500);
+                res.statusStatus(500);
             }
             else
             {
@@ -82,51 +85,76 @@ app.post("/addUser", (req, res) => {
 
 app.post("/favorite", (req, res) => {
     try {
+        let dir = path.join(__dirname, '/log.txt');
+        fs.appendFileSync(dir, 'entered /favorite \n');
         const conn = getConn();
+        fs.appendFileSync(dir, 'got conn \n');
         conn.connect();
+        fs.appendFileSync(dir, 'connected to db \n');
 
-        let insertQuery = 'INSERT INTO ?? (??,??) VALUES (?,?)';
-        let query = mysql.format(insertQuery,["Favorites", "userid", "petid", req.body.userid, req.body.petid]);
+        let deleteQuery = 'DELETE FROM ?? WHERE userid = ? AND petid = ?';
+        let query = mysql.format(deleteQuery,["Favorites", req.body.userid, req.body.petid]);
         conn.query(query,(err, response) => {
+            fs.appendFileSync(dir, 'ran query ' + query + '\n');
             if(err) {
-                res.send(500);
+                res.sendStatus(500);
             }
             else
             {
-                res.send(200);
+                let insertQuery2 = 'INSERT INTO ?? (??,??) VALUES (?,?)';
+                let query2 = mysql.format(insertQuery,["Favorites", "userid", "petid", req.body.userid, req.body.petid]);
+                conn.query(query2,(err2, response) => {
+                    fs.appendFileSync(dir, 'ran query ' + query2 + '\n');
+                    if(err2) {
+                        res.sendStatus(500);
+                    }
+                    else
+                    {
+                        res.sendStatus(200);
+                    }
+                });
             }
         });
+        fs.appendFileSync(__dirname + '/log.txt', 'conn.end \n');
         conn.end();
     } catch (err) {
+        fs.appendFileSync(__dirname + '/log.txt', 'got error ' + err.message);
         next(err);
     }
 });
 
 app.post("/unfavorite", (req, res) => {
     try {
+        let dir = path.join(__dirname, '/log.txt');
+        fs.appendFileSync(dir, 'entered /favorite \n');
         const conn = getConn();
+        fs.appendFileSync(dir, 'got conn \n');
         conn.connect();
+        fs.appendFileSync(dir, 'connected to db \n');
         
         let deleteQuery = 'DELETE FROM ?? WHERE userid = ? AND petid = ?';
         let query = mysql.format(deleteQuery,["Favorites", req.body.userid, req.body.petid]);
         conn.query(query,(err, response) => {
+            fs.appendFileSync(dir, 'ran query ' + query + '\n');
             if(err) {
-                res.send(500);
+                res.sendStatus(500);
             }
             else
             {
-                res.send(200);
+                res.sendStatus(200);
             }
         });
+        fs.appendFileSync(__dirname + '/log.txt', 'conn.end \n');
         conn.end();
     } catch (err) {
+        fs.appendFileSync(__dirname + '/log.txt', 'got error ' + err.message);
         next(err);
     }
 });
 
 app.get("/", (req, res) => {
     let dir = path.join(__dirname, '/log.txt');
-    res.send("HELLO6 dir=|" + dir + "|" );
+    res.send("HELLO8 dir=|" + dir + "|" );
 });
 
 let PORT = process.env.PORT || 3000;
